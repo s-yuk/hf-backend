8803d6c65c12d0299a6b6075ff743c2602196295
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-12-01 02:36:05 +0000

    refresh token周りでエラー

--- api/src/main/java/com/example/api/security/SecurityConfig.java
+++ api/src/main/java/com/example/api/security/SecurityConfig.java
@@ -32,12 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
     CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-    customAuthenticationFilter.setFilterProcessesUrl("/api/login/**");
+    customAuthenticationFilter.setFilterProcessesUrl("/api/login");
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(STATELESS);
-    http.authorizeRequests().antMatchers("/login").permitAll();
+    http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
     http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
     http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
     http.authorizeRequests().anyRequest().authenticated();
