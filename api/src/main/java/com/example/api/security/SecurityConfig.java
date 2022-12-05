9d5188a2f5a1640f6739b1710d2f2b8a439d8743
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-12-05 13:58:19 +0000

    corsエラー解消

--- api/src/main/java/com/example/api/security/SecurityConfig.java
+++ api/src/main/java/com/example/api/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.api.filter.CustomAuthenticationFilter;
 import com.example.api.filter.CustomAuthorizationFilter;
@@ -18,6 +22,8 @@ import lombok.RequiredArgsConstructor;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+import java.util.Arrays;
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,16 +39,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
     customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+    http.csrf().disable();
+    http.sessionManagement().sessionCreationPolicy(STATELESS);
     http
-      .csrf().disable()
-      .sessionManagement().sessionCreationPolicy(STATELESS);
-    http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
-    http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
-    http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+      .authorizeRequests()
+        .antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
+        .antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER")
+        .antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
     http.authorizeRequests().anyRequest().authenticated();
     http.addFilter(customAuthenticationFilter);
     http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.cors(Customizer.withDefaults());
   }
+
+  @Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
   @Bean
   @Override
