8a014b65d682710d6b587852891514253b4e7f22
s-yuk <100174001+s-yuk@users.noreply.github.com>
2022-12-13 02:44:08 +0000

    typo

--- api/src/main/java/com/example/api/auth/service/Impl/ResourceServiceImpl.java
+++ api/src/main/java/com/example/api/auth/service/Impl/ResourceServiceImpl.java
@@ -4,7 +4,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.example.api.auth.service.ResouceService;
+import com.example.api.auth.service.ResourceService;
 import com.example.api.model.Role;
 import com.example.api.repo.RoleRepo;
 
