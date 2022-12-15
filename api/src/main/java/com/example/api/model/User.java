d45172a3df1724ea7c5ec1321756fed14eefe015
“s-yuk” <100174001+s-yuk@users.noreply.github.com>
2022-12-15 14:38:18 +0000

    一緒にやっていくためコメントアウト

--- api/src/main/java/com/example/api/model/User.java
+++ api/src/main/java/com/example/api/model/User.java
@@ -1,79 +1,79 @@
-package com.example.api.model;
+// package com.example.api.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
+// import java.util.ArrayList;
+// import java.util.Collection;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.FetchType;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.JoinColumn;
+// import javax.persistence.JoinTable;
+// import javax.persistence.ManyToMany;
+// import javax.persistence.Table;
+// import javax.persistence.Transient;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "user")
-public class User implements UserDetails {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")
-  private Long id;
-  @Column(name = "email", unique = true)
-  private String email;
-  @Column(name = "username", unique = true)
-  private String username;
-  @Column(name = "password")
-  private String password;
-  @Column(name = "have_points", columnDefinition = "int default '0'")
-  private Integer have_points;
-  @Column(name = "have_stocks")
-  private Integer have_stocks;
-  // @Column(name = "grop_id")
-  // private Integer grop_id;
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Table(name = "user")
+// public class User implements UserDetails {
+//   @Id
+//   @GeneratedValue(strategy = GenerationType.IDENTITY)
+//   @Column(name = "user_id")
+//   private Long id;
+//   @Column(name = "email", unique = true)
+//   private String email;
+//   @Column(name = "username", unique = true)
+//   private String username;
+//   @Column(name = "password")
+//   private String password;
+//   @Column(name = "have_points", columnDefinition = "int default '0'")
+//   private Integer have_points;
+//   @Column(name = "have_stocks")
+//   private Integer have_stocks;
+//   // @Column(name = "grop_id")
+//   // private Integer grop_id;
 
-  @ManyToMany(fetch = FetchType.EAGER)
-  @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-  private Collection<Role> roles = new ArrayList<>();
+//   @ManyToMany(fetch = FetchType.EAGER)
+//   @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+//   private Collection<Role> roles = new ArrayList<>();
 
-  @Transient
-  private Collection<GrantedAuthority> authorities;
+//   @Transient
+//   private Collection<GrantedAuthority> authorities;
 
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return this.authorities;
-  }
+//   @Override
+//   public Collection<? extends GrantedAuthority> getAuthorities() {
+//     return this.authorities;
+//   }
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
+//   @Override
+//   public boolean isAccountNonExpired() {
+//     return true;
+//   }
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
+//   @Override
+//   public boolean isAccountNonLocked() {
+//     return true;
+//   }
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
+//   @Override
+//   public boolean isCredentialsNonExpired() {
+//     return true;
+//   }
 
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
-}
+//   @Override
+//   public boolean isEnabled() {
+//     return true;
+//   }
+// }
