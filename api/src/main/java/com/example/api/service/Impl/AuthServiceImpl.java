340a3d7aaf8b1a4b7864f5f83fe0c3988153c6c7
“s-yuk” <100174001+s-yuk@users.noreply.github.com>
2022-12-22 13:18:40 +0000

    ✅ signup完成

--- api/src/main/java/com/example/api/service/Impl/AuthServiceImpl.java
+++ api/src/main/java/com/example/api/service/Impl/AuthServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.api.model.Role;
 import com.example.api.model.entity.User;
 import com.example.api.model.form.SignUpForm;
 import com.example.api.repo.UserRepo;
@@ -20,24 +21,30 @@ import lombok.extern.slf4j.Slf4j;
 public class AuthServiceImpl implements AuthService {
   @Autowired
   private UserRepo userRepo;
-
   private final PasswordEncoder passwordEncoder;
 
   public String signUp(SignUpForm form) {
+    // 親か子どもの判定
+    String userType = form.getRole();
+
     User user = new User();
+    user.setId(UUID.randomUUID().toString());
     user.setUsername(form.getUsername());
     user.setEmail(form.getEmail());
-    user.setPassword(form.getPassword());
-    // user.setRole(form.getRole());
-
+    user.setPassword(passwordEncoder.encode(form.getPassword()));
+    switch (userType) {
+      case "1":
+        user.setRole(Role.CHILD);
+        break;
+      case "2":
+        user.setRole(Role.PARENT);
+        break;
+    }
     boolean userExists = userRepo.findByEmail(user.getEmail()).isPresent();
     if (userExists) {
       throw new IllegalStateException("The email address you entered is already in use");
-    } else {
-      user.setId(UUID.randomUUID().toString());
-      user.setPassword(passwordEncoder.encode(user.getPassword()));
-      userRepo.save(user);
     }
+    userRepo.save(user);
     return user.getId();
   }
 }
--- api_user.md
+++ api_user.md
@@ -1,8 +1,15 @@
 ## 新規登録
 ```
 POST http://localhost:8080/api/register
-
+{
+  username: "username",
+  email: "email",
+  password: "password",
+  role: "role" 1なら子ども 2なら親
+}
 @return
 body id
 header access_token
 ```
+
+## ユーザー情報変更
