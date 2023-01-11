e9e7fab9dad1ca0d8889c9a1b853f4d90694e9f4
fujikawa <102748298+kode0221@users.noreply.github.com>
2023-01-11 00:59:01 +0000

    商品削除できたかも

--- api/src/main/java/com/example/api/controller/ProductController.java
+++ api/src/main/java/com/example/api/controller/ProductController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,13 @@ public class ProductController {
     product.setName(form.getName());
     product.setUsePoint(form.getUsePoint());
     productRepo.save(product);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  // 商品削除
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteProductEntity(@PathVariable Long id) {
+    productRepo.deleteById(id);
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
